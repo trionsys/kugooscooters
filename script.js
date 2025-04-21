fda6dcc44fd96269e62682ea1c86e283236014c6
tri0n111 <robloxrar@gmail.com>
2025-04-21 20:22:57 +0400

    тест123

---
+++ script.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    //   },
+
+  });
+
